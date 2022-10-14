be51f279dd4d72ba8136852e88c5c8ffef7d01e7
pstnv <pstnv@mail.ru>
2022-10-14 23:52:08 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { library } from './library';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+function App () {
+    let [sign, setSign] = useState(0);
+    const {id, name, date, image} = library[sign];
+    const [isLoading, setIsLoading] = useState(true);
+    const [astro, setAstro] = useState('');
+
+
+
+    useEffect (() => {
+        const fetchData = async () => {        
+            const url = `https://aztro.sameerkumar.website/?sign=${name}&day=today`;
+            const response = await fetch(url, {method: 'POST'});
+            const data = await response.json();
+            const astro = data.description;
+            // console.log(astro);
+            if (astro) {
+                setIsLoading(false);
+                setAstro(data.description);
+            }
+            else {
+                setIsLoading(true);
+            }  
+        } 
+        
+        fetchData()
+    // to catch any error
+        .catch(console.error);
+
+    },[name]);
+    // },[sign]); либо так
+
+    const previousSign = () => {
+        setIsLoading(true);
+        setSign(sign => {
+          sign--;
+            return sign <= 0 ? library.length - 1 :  sign;
+        })
+    }
+
+    const nextSign = () => {
+        setIsLoading(true);
+        setSign(() => {
+            return sign >= library.length ? 0 : sign++;
+        })
+    }
+
+
+    return (
+        <div className="main">
+            <div className="container">
+                <div className="horoscope">
+                    <div className="sign">
+                        <img className="sign_image" src={image} alt="zodiac sign" />
+                        <div className="choice">
+                            <button className="choice_btn" onClick={() => previousSign()} id="prev"><img className="choice_image" src="./img/icons/arrow_prev.png" alt="previous" /></button>
+                            <div className="choise_container">
+                                <h2 className="choice_header">{name}</h2>
+                                <div className="choice_date">{date}</div>
+                            </div>
+                            <button className="choice_btn" onClick={() => nextSign()} id="next"><img className="choice_image" src="./img/icons/arrow_next.png" alt="next" /></button>
+                        </div>
+                    </div>
+                    <div className="description">
+                        <h1 className="description_header">Daily Horoscope</h1>
+                        <p className="description_text"> {isLoading ? 'Loading...' : astro} </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
 
 export default App;
---
+++ src/library.js
@@ -0,0 +1,74 @@
+export const library = [
+    {
+        id: 1,
+        name: 'aries',
+        date: 'Mar 21 - Apr 20',
+        image: './img/logo/Aries.webp'
+    },
+    {
+        id: 2,
+        name: 'taurus',
+        date: 'Apr 21 - May 21',
+        image: './img/logo/Taurus.webp'
+    },
+    {
+        id: 3,
+        name: 'gemini',
+        date: 'May 22 - Jun 21',
+        image: './img/logo/Gemini.webp'
+    },
+    {
+        id: 4,
+        name: 'cancer',
+        date: 'Jun 22 - Jul 22',
+        image: './img/logo/Cancer.webp'
+    },
+    {
+        id: 5,
+        name: 'leo',
+        date: 'Jul 23 - Aug 21',
+        image: './img/logo/Leo.webp'
+    },
+    {
+        id: 6,
+        name: 'virgo',
+        date: 'Aug  22 - Sep 23',
+        image: './img/logo/Virgo.webp'
+    },
+    {
+        id: 7,
+        name: 'libra',
+        date: 'Sep 24 - Oct 23',
+        image: './img/logo/Libra.webp'
+    },
+    {
+        id: 8,
+        name: 'scorpio',
+        date: 'Oct 24 - Nov 22',
+        image: './img/logo/Scorpio.webp'
+    },
+    {
+        id: 9,
+        name: 'sagittarius',
+        date: 'Nov 23 - Dec 22',
+        image: './img/logo/Sagittarius.webp'
+    },
+    {
+        id: 10,
+        name: 'capricorn',
+        date: 'Dec 23 - Jan 20',
+        image: './img/logo/Capricorn.webp'
+    },
+    {
+        id: 11,
+        name: 'aquarius',
+        date: 'Jan 21 - Feb 19',
+        image: './img/logo/Aquarius.webp'
+    },
+    {
+        id: 12,
+        name: 'pisces',
+        date: 'Feb 20 - Mar 20',
+        image: './img/logo/Pisces.webp'
+    },
+]
